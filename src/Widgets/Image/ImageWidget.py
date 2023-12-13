69e17159ccb3ddee23428d98667f64e8ee2b2069
Mateusz Cebula <149671545+cebulamateusz@users.noreply.github.com>
2023-12-13 13:43:34 +0100

    36 dynamic grid in gridview (#37)
    * Created basic project structure. Added Widgets and templates. Created possibility to change widgets.
    
    * Modified gridview to enable dynamic grid functionality.

--- src/Widgets/Image/ImageWidget.py
+++ src/Widgets/Image/ImageWidget.py
@@ -4,8 +4,26 @@ from src.Widgets.Image.ImageView import Ui_Image
 
 
 class ImageWidget(QWidget, Ui_Image):
+    __image_path: str = None
 
     def __init__(self, *args, obj=None, **kwargs):
         super(ImageWidget, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.lbImage.mousePressEvent = self.get_normalized_position
+        self.pbLoad.clicked.connect(self.loadImage)
 
+    def get_normalized_position(self, event):
+        mouse_pos = event.pos()
+        label_width = self.lbImage.width()
+        label_height = self.lbImage.height()
+
+        self.__normalized_mp = (mouse_pos.x() / label_width, mouse_pos.y() / label_height)
+        # todo: pass coordinates to image and refresh it
+
+        print(f"Relative position within label: ({self.__normalized_mp[0]}, {self.__normalized_mp[1]})")
+
+    def loadImage(self):
+        path, _ = QFileDialog.getOpenFileName(self, "Select an image file",  "", "PNG (*.png);;JPG (*.jpg);;All Files (*)")
+        if path:
+            self.__image_path = path
+        print(self.__image_path)
