830bc6497e139d5341447b8b37bc61f760bc1ddc
mateceb250 <mateceb250@student.polsl.pl>
2023-12-15 01:02:37 +0100

    fixes to dark mode

--- src/main.py
+++ src/main.py
@@ -12,10 +12,70 @@ def global_exception_handler():
     logging.error(error_message)
 
 app = QApplication(sys.argv)
-file = QFile(".\\darkstyle.qss")
-file.open(QFile.ReadOnly | QFile.Text)
-stream = QTextStream(file)
-app.setStyleSheet(stream.readAll())
+style = '''
+        /* Dark Mode Style Sheet */
+
+/* Set application-wide properties */
+QWidget {
+    background-color: #1e1e1e;
+    color: #f0f0f0;
+    font-size: 12px;
+}
+
+/* Button styles */
+QPushButton {
+    background-color: #353535;
+    color: #f0f0f0;
+    border: 1px solid #505050;
+    border-radius: 4px;
+    padding: 5px;
+}
+
+QPushButton:hover {
+    background-color: #454545;
+}
+
+/* Toggle Button styles */
+QPushButton:checked {
+    background-color: #007acc;
+    color: #f0f0f0;
+}
+
+/* Line Edit styles */
+QLineEdit {
+    background-color: #2e2e2e;
+    color: #f0f0f0;
+    border: 1px solid #505050;
+    border-radius: 4px;
+    padding: 5px;
+}
+
+/* Scroll Bar styles */
+QScrollBar:vertical {
+    border: none;
+    background: #1e1e1e;
+    width: 14px;
+}
+
+QScrollBar::handle:vertical {
+    background: #3a3a3a;
+    min-height: 20px;
+}
+
+QScrollBar::handle:vertical:hover {
+    background: #444444;
+}
+
+QScrollBar::add-line:vertical {
+    background: none;
+}
+
+QScrollBar::sub-line:vertical {
+    background: none;
+}
+
+        '''
+app.setStyleSheet(style)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 sys.excepthook = global_exception_handler
